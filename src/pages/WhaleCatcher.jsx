19756aa5f95b0a1d2cf5ea2bffa3e89a8bcb9d30
ken-chan <work.kenchan@gmail.com>
2024-03-19 14:57:01 +0800

    add comments

--- src/pages/WhaleCatcher.jsx
+++ src/pages/WhaleCatcher.jsx
@@ -19,8 +19,8 @@ const WhaleCatcher = () => {
   const [tokenSymbol, setTokenSymbol] = useState("PEPE");
   const [tokenPrice, setTokenPrice] = useState(0.0);
 
-  // const selectedRef = useRef();
 
+  // to access api 1second at a time
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
@@ -30,6 +30,7 @@ const WhaleCatcher = () => {
     setStopLoop(true)
   }
   
+  // setting of data before running the actual loop
   function runLoopFunction(){
     playSound()
     setStartSpinner(true)
@@ -38,8 +39,11 @@ const WhaleCatcher = () => {
     let exchangesAddress = "";
     const contractAddress=tokenAddress
 
+    // contract address is the token address you wanted to monitor.
     if (contractAddress ==="0x6982508145454Ce325dDbE47a25d4ec3d2311933"){
       exchangesAddress=exchangesInPepeToken
+
+      // an extraction of "address" only to this array for comparison purpose.
       exchangeAddressesOnly=Object.values(exchangesInPepeToken).map(function(d){
         return d["address"].toLowerCase()        
       })
@@ -51,21 +55,31 @@ const WhaleCatcher = () => {
       })
     }
 
+    // get price from api using contract address. e.g. get price of pepe inu
     const price=coingeckoPriceApi(contractAddress)
+
+    // set token price to useState
     setTokenPrice(price)
 
+    // go into loop
     loopFunction(price, 0,exchangeAddressesOnly, exchangesAddress)    
   }
 
+  // this will be the recursive loop running the main function catchWhale
+  // which calls etherscan api to fetch latest 25 transaction of (e.g. pepe token)
   function loopFunction(price, counter, exchangeAddressesOnly, exchangesAddress) {
 
+    // the loop will perform a cycle per cycle
     sleep(1000).then(async () => {
       counter++
       if (counter >=5){
         counter=0
 
+        // get price of token using contract address
         price= await coingeckoPriceApi(tokenAddress)
 
+        // take the result of the whalecatcher list and refresh price if price
+        // is previously "pending" as it is a async await api call.
         let temp=[...offloadingWhale]
         for (const datum in temp){
           if (price!=undefined){
@@ -74,12 +88,20 @@ const WhaleCatcher = () => {
             }
           }
         }
+
+        // after updating data, set to state.
         setOffloadingWhale(temp)
+
+        // after getting the price, set to state.
         setTokenPrice(price)
       }
 
       if (stopLoop===false){
+        // catch whale (get 25 latest transactions of (e.g. pepe token) and
+        // compared to list of crypto exchange address  )
         catchWhale(price, exchangeAddressesOnly, exchangesAddress)
+
+        // recursion
         loopFunction(price, counter, exchangeAddressesOnly, exchangesAddress);
       }
       else
@@ -87,41 +109,66 @@ const WhaleCatcher = () => {
     });
   }
 
+  // catch whale (get 25 latest transactions of (e.g. pepe token) and
+  // compared to list of crypto exchange address  )
   const catchWhale = async (price, exchangesAdd, exchangesAddress) => {
 
-    const contractAddress=tokenAddress
-    const totalTransaction=25 // standard 
+    //set to contract address what is previously set stated in tokenAddress
+    const contractAddress=tokenAddress    
+    const totalTransaction=25 // standard page list 25 transactions.
     
+    // basic business logic check !=""
     if (contractAddress != "") {
       let url = "";
       url = `https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=${contractAddress}&page=1&offset=${totalTransaction}&startblock=0&endblock=99999999&sort=desc&apikey=${import.meta.env.VITE_YourApiKeyToken}`  
 
+      // fetching etherscan api
       const res  = await fetch(url)
       if (res.ok) {
         let data = await res.json();
-        setCatchingWhale(data);
+        setCatchingWhale(data); // not useful for now
+
+        // cycle thru the 25 transactions extracted from etherscan api
         for (const datum in data["result"]){
+
+          // get the "To" address, to be compared against a list of
+          // crypto exchange wallet address.  This is to check if any
+          // whale is trying to transfer their token(e.g. pepe token)
+          // to exchange, to be sell off.
           const toAddress = data["result"][datum].to
 
+          // multiple decimalValue with value to get qty of (e.g. pepe token)
           const tokenDecimal= data["result"][datum].tokenDecimal
           const decimalValue= (1.0/(Math.pow(10,tokenDecimal)))
 
-          if (data["result"][datum].confirmations <=200){ // don't take too long ago data
-              
+          // don't take too long ago data
+          // when sending to crypto exchange, usually needs about
+          // 20 to 30 confirmations before you can sell the token
+          // you have transferred.  during this time(around 3mins),
+          // before the confirmations completes, user of this app can
+          // 1) sell their own (e.g. pepe token)
+          // 2) short the (e.g. pepe token)
+          if (data["result"][datum].confirmations <=200){ 
+            // obsolete - to be tested later and discard
             const block=Object.values(offloadingWhale).map(function(d){
               return d["blockNumber"]
             })
 
+              // check all crypto exchange wallet address, any of them got this
+              // transaction's "To" address. essentially, asking if holder sending
+              // to crypto exchange?
               if (Object.values(exchangesAdd).includes(toAddress)){
 
-                // AMOUNT
+                // AMOUNT in $$$
                 let amountInQty=decimalValue*data["result"][datum].value
                 data["result"][datum].amountInQty=amountInQty
                 let sumInDollar=price*amountInQty
 
                 // if (sumInDollar >=0){
+                  // display currency number nicely
                   sumInDollar= "$" + sumInDollar.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
+                  // if price is undefined, sumInDollar will be NaN
                   if (isNaN(sumInDollar)){
                     data["result"][datum].sumInDollar= "sync..."
                   }
@@ -133,6 +180,10 @@ const WhaleCatcher = () => {
                   const indexAdd=Object.values(exchangesAdd).indexOf(toAddress)
                   const targetedExchangeName=exchangesAddress[indexAdd].name
                   data["result"][datum].targetedExchangeName= targetedExchangeName;
+
+
+                  // check offloadingWhale list of transaction's block number against incoming
+                  // 25 transaction's blocknumber, if same, don't add to offloadingWhale
                   const block=Object.values(offloadingWhale).map(function(d){
                     return d["blockNumber"]
                   })
@@ -155,6 +206,7 @@ const WhaleCatcher = () => {
     }  
   } // end of catchWhale
 
+  // cmc api to get price. got cors error.
   const cmcPriceApi = async (contractName) => {
 
     if (contractName != "") {
@@ -187,7 +239,7 @@ const WhaleCatcher = () => {
     }  
   } // end of cmcPriceApi 
 
-
+  //coingecko get price of token using contract address
   const coingeckoPriceApi = async (contractAdd) => {
     try{
       if (contractAdd != "") {
@@ -226,14 +278,18 @@ const WhaleCatcher = () => {
     setOffloadingWhale(temp)
   }
 
+  // select element change
   const handleSelectChange = (event) => {
+    // use selected index of element to get "name and address" of option item details
     const name = event.target.options[event.target.selectedIndex].dataset.name
     const address = event.target.options[event.target.selectedIndex].dataset.address
     
+    // set to state
     setTokenSymbol(name)    
     setTokenAddress(address)
   }
   
+  // obsolete - to be tested later and discard
   useEffect(() => {
     const controller = new AbortController();
   
@@ -242,6 +298,7 @@ const WhaleCatcher = () => {
     };
   }, []);
 
+  // obsolete - to be tested later and discard
   useEffect(() => {
     const controller = new AbortController();
   
